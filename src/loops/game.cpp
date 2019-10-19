714416d3df1716bd28cde93a01caa1b3d9d5b8f5
Ben Russell <thematrixeatsyou@gmail.com>
2019-10-20 12:04:03 +1300

    Make player moveable

--- src/loops/game.cpp
+++ src/loops/game.cpp
@@ -8,16 +8,43 @@
 
 namespace loops
 {
+	int player_x = 320-12;
+	int player_y = 180-12;
+	Direction player_direction = direction::SOUTH;
 }
 
 loops::MainLoopState loops::game_loop(bool state_changed)
 {
 	(void)state_changed;
 
-	if(key[KEY_ESC]) {
+	//
+	// Update input
+	//
+	if (key[KEY_ESC]) {
 		return mainloop::EXIT;
 	}
 
+	int player_dx = 0;
+	int player_dy = 0;
+	if (key[KEY_UP]) { player_dy -= 1; }
+	if (key[KEY_DOWN]) { player_dy += 1; }
+	if (key[KEY_LEFT]) { player_dx -= 1; }
+	if (key[KEY_RIGHT]) { player_dx += 1; }
+
+	//
+	// Update logic
+	//
+	if (player_dy < 0) { player_direction = direction::NORTH; }
+	if (player_dy > 0) { player_direction = direction::SOUTH; }
+	if (player_dx < 0) { player_direction = direction::WEST; }
+	if (player_dx > 0) { player_direction = direction::EAST; }
+
+	player_x += player_dx;
+	player_y += player_dy;
+
+	//
+	// Draw
+	//
 	clear_to_color(backbuf, makecol(85,85,255));
 	textprintf_ex(backbuf, font, 20, 10+8*0,
 		makecol(255,255,0), -1,
@@ -29,7 +56,9 @@ loops::MainLoopState loops::game_loop(bool state_changed)
 		makecol(255,255,0), -1,
 		"80 columns barely fit: TICK!");
 
-	draw_sprite(backbuf, gfx::player_gfx[direction::SOUTH], 320-12, 180-12);
+	draw_sprite(backbuf,
+		gfx::player_gfx[player_direction],
+		player_x, player_y);
 
 	return mainloop::GAME;
 }
