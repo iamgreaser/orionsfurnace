1b13e37f8deaf3dcae0690af92e1a342e78b2d0d
Ben Russell <thematrixeatsyou@gmail.com>
2019-10-29 03:34:46 +0000

    Move game drawing code into Game

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -20,6 +20,8 @@ along with Orion's Furnace.  If not, see <https://www.gnu.org/licenses/>.
 #include "core/core.h"
 #include "core/helpers.h"
 #include "core/save.h"
+#include "gfx/gfx.h"
+#include "gfx/sprite.h"
 
 #include <cstdint>
 #include <iostream>
@@ -93,6 +95,30 @@ void Game::tick(const GameFrame &game_frame)
 
 void Game::draw(void)
 {
+  // Calculate relative cell offset
+  // TODO: find a consistent way of biasing against truncation division
+  int rel_px = gfx::cam_px;
+  int rel_py = gfx::cam_py;
+  int rel_cx = (rel_px < 0 ? (rel_px-(24-1))/24 : rel_px/24);
+  int rel_cy = (rel_py < 0 ? (rel_py-(24-1))/24 : rel_py/24);
+  int width = this->get_width();
+  int height = this->get_height();
+
+  // Draw tiles
+  for (int cy = 0 + rel_cy; cy < CAM_H_CELLS+1 + rel_cy; cy++) {
+    if (cy < 0) { continue; }
+    if (cy >= height) { break; }
+    int py = CAM_Y + (cy*CELL_H);
+
+    for (int cx = 0 + rel_cx; cx < CAM_W_CELLS+1 + rel_cx; cx++) {
+      if (cx < 0) { continue; }
+      if (cx >= width) { break; }
+      int px = CAM_X + (cx*CELL_W);
+
+      gfx::tile_gfx_floor.draw(px, py);
+    }
+  }
+
   for (Player &p : m_players) {
     p.draw();
   }
--- src/loops/game.cpp
+++ src/loops/game.cpp
@@ -219,28 +219,6 @@ void GameLoop::draw_playfield(void)
 
     gfx::set_camera(rel_px, rel_py);
 
-    // Calculate relative cell offset
-    // TODO: find a consistent way of biasing against truncation division
-    int rel_cx = (rel_px < 0 ? (rel_px-(24-1))/24 : rel_px/24);
-    int rel_cy = (rel_py < 0 ? (rel_py-(24-1))/24 : rel_py/24);
-    int width = game.get()->get_width();
-    int height = game.get()->get_height();
-
-    // Draw tiles
-    for (int cy = 0 + rel_cy; cy < CAM_H_CELLS+1 + rel_cy; cy++) {
-      if (cy < 0) { continue; }
-      if (cy >= height) { break; }
-      int py = CAM_Y + (cy*CELL_H);
-
-      for (int cx = 0 + rel_cx; cx < CAM_W_CELLS+1 + rel_cx; cx++) {
-        if (cx < 0) { continue; }
-        if (cx >= width) { break; }
-        int px = CAM_X + (cx*CELL_W);
-
-        gfx::tile_gfx_floor.draw(px, py);
-      }
-    }
-
     if (game != nullptr) {
       game.get()->draw();
     }
